dce517c88d587b09f180fb3314fb9d3806c671a9
Prasenjit <p2dhara@gmail.com>
2024-01-22 10:14:14 +0100

    Using class and subclasses for estimation of radio map in 1d

--- map_estimator/map_estimators.py
+++ map_estimator/map_estimators.py
@@ -1,67 +1,68 @@
 from sklearn.neighbors import KNeighborsRegressor
-from map_generator_1d import MapGenerator1D
+from map_generator.map_generator_1d import MapGenerator1D
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import sys
+import matplotlib.pyplot as plt
 
-class MapEstimator:
-    def __init__(self, v_measurement_locs, v_measurements, v_grid_x):
-        self.v_measurement_locs = v_measurement_locs
-        self.v_measurements = v_measurements
-        self.v_grid_x = v_grid_x
+""" class MapEstimator:
+    def __init__(self): # 
+        # self.v_measurement_locs = v_measurement_locs
+        # self.v_measurements = v_measurements
+        # self.v_grid_x = v_grid_x
+        pass
         
     def estimate(self, v_measurement_locs, v_measurements, v_grid_x):
         # write common funcitons
         return self._estimate(v_measurement_locs, v_measurements, v_grid_x)
     
     def _estimate(self, v_measurement_locs, v_measurements, v_grid_x):
-        raise NotImplementedError
+        raise NotImplementedError """
 
 
-class PolynomialEstimator(MapEstimator):
-    def __init__(self, v_measurement_locs, v_measurements, v_grid_x, degree=5):
-        super().__init__(v_measurement_locs, v_measurements, v_grid_x)
+class PolynomialEstimator():
+    def __init__(self, degree=5):
+        # super().__init__(v_measurement_locs, v_measurements, v_grid_x)
         self.degree = degree
     
-    def estimate(self):
-        v_meas_locs_poly = np.vander(self.v_measurement_locs, self.degree +1, increasing = True)
-        estimated_coeff = np.linalg.lstsq(v_meas_locs_poly, self.v_measurements, rcond = None)[0]
-        v_grid_x_poly = np.vander(self.v_grid_x, self.degree + 1, increasing = True)
+    def estimate(self, v_measurement_locs, v_measurements, v_grid_x):
+        v_meas_locs_poly = np.vander(v_measurement_locs, self.degree +1, increasing = True)
+        estimated_coeff = np.linalg.lstsq(v_meas_locs_poly, v_measurements, rcond = None)[0]
+        v_grid_x_poly = np.vander(v_grid_x, self.degree + 1, increasing = True)
         return np.dot(v_grid_x_poly, estimated_coeff)
 
 
-class LinearInterpEstimator(MapEstimator):
-    def __init__(self, v_measurement_locs, v_measurements, v_grid_x):
-        super().__init__(v_measurement_locs, v_measurements, v_grid_x)
+class LinearInterpEstimator():
+    # def __init__(self, v_measurement_locs, v_measurements, v_grid_x):
+    #     super().__init__(v_measurement_locs, v_measurements, v_grid_x)
     
-    def estimate(self):
-        estimated_power = np.zeros(self.v_grid_x.shape)
-        for i in range(len(self.v_grid_x)):
-            k = np.searchsorted(self.v_measurement_locs, self.v_grid_x[i], side = 'right')
+    def estimate(self, v_measurement_locs, v_measurements, v_grid_x):
+        estimated_power = np.zeros(v_grid_x.shape)
+        for i in range(len(v_grid_x)):
+            k = np.searchsorted(v_measurement_locs, v_grid_x[i], side = 'right')
             
             if k == 0:
-                estimated_power[i] = self.v_measurements[0]
-            elif k == len(self.v_measurements):
-                estimated_power[i] = self.v_measurements[-1]
+                estimated_power[i] = v_measurements[0]
+            elif k == len(v_measurements):
+                estimated_power[i] = v_measurements[-1]
             else:
-                x_left, x_right = self.v_measurement_locs[k-1], self.v_measurement_locs[k]
-                y_left, y_right = self.v_measurements[k-1], self.v_measurements[k]
-                estimated_power[i] = y_left + (self.v_grid_x[i] - x_left)*(y_right - y_left)/(x_right - x_left)        
+                x_left, x_right = v_measurement_locs[k-1], v_measurement_locs[k]
+                y_left, y_right = v_measurements[k-1], v_measurements[k]
+                estimated_power[i] = y_left + (v_grid_x[i] - x_left)*(y_right - y_left)/(x_right - x_left)        
         return estimated_power
     
 
-class KnnEstimator(MapEstimator):
-    def __init__(self, v_measurement_locs, v_measurements, v_grid_x, num_neighbors=5):
-        super().__init__(v_measurement_locs, v_measurements, v_grid_x)
+class KnnEstimator():
+    def __init__(self, num_neighbors=5): # , v_measurement_locs, v_measurements, v_grid_x        
         self.num_neighbors = num_neighbors
         self.knn_reg = KNeighborsRegressor(n_neighbors=self.num_neighbors, weights='distance')
 
-    def estimate(self):
-        self.knn_reg.fit(self.v_measurement_locs.reshape(-1, 1), self.v_measurements.reshape(-1, 1))
-        return self.knn_reg.predict(self.v_grid_x.reshape(-1, 1)).ravel()
+    def estimate(self, v_measurement_locs, v_measurements, v_grid_x):
+        self.knn_reg.fit(v_measurement_locs.reshape(-1, 1), v_measurements.reshape(-1, 1))
+        return self.knn_reg.predict(v_grid_x.reshape(-1, 1)).ravel()
     
-class DnnEstimator(MapEstimator):
+class DnnEstimator():
     def __init__(self, input_layer_size, hidden_layers_size, output_layer_size):
         self.model = self.build_model(input_layer_size, hidden_layers_size, output_layer_size)      
     
@@ -75,9 +76,20 @@ class DnnEstimator(MapEstimator):
         model.compile(optimizer = 'adam', loss = 'mse')
         return model            
         
-    def train_model(self, X_train, y_train, epochs = 100, batch_size = 32):
-        self.model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size)
+    def train_model(self, X_train, y_train, epochs = 100, validation_split = 0.25, batch_size = 32):
+        return self.model.fit(X_train, y_train, epochs = epochs, validation_split = validation_split, batch_size = batch_size)
     
+    def plot_training_history(self, history):
+        plt.plot(history.history['loss'], label = 'Training Loss')
+        if 'val_loss' in history.history:
+            plt.plot(history.history['val_loss'], label = 'Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Training and Validation Loss')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
     def save_weights(self, file_name):
         self.model.save_weights(file_name)
         
@@ -85,8 +97,23 @@ class DnnEstimator(MapEstimator):
         self.model.load_weights(file_name)
         
     def estimate(self, X_test):        
-        return self.model.predict(X_test)        
-        
+        return self.model.predict(X_test) 
+
+def plot_true_and_estimated_maps(v_x, v_true_map, v_estimated_map, v_meas_locs, v_meas, fig_title = "KNN"): # v_x, v_received_power, v_estimated_map, v_measurement_locs, v_measurements
+    fig, ax = plt.subplots()
+    ax.plot(v_x, v_true_map, "b--", label = "True Map")      
+    ax.plot(v_x, v_estimated_map, "r-", label= "Estimated Map")
+    ax.stem(v_meas_locs, v_meas, linefmt='g-', markerfmt='gx', basefmt=' ',
+        label='Measurements', use_line_collection=False)
+    ax.set_xlabel("x[m]")
+    ax.set_ylabel("Power")
+    ax.set_title(f'True and Estimated Map Using {fig_title}')
+    ax.legend()
+    plt.grid()
+    plt.show()
+    return fig
+
+"""        
 sys.path.append('map_generator')
 
 # Initialize parameters
@@ -96,6 +123,7 @@ mapGen = MapGenerator1D(
     r_x_min = 1,
     r_x_max = 100
 )
+
 
 num_points = 100
 num_maps = 1
@@ -116,12 +144,8 @@ v_measurements = mapGen.generate_measurement_data(v_x, v_received_power, v_noise
 
 # KNN estimation
 num_neighbors=5
-knn_estimation = KnnEstimator(v_measurement_locs, 
-                              v_measurements, 
-                              v_x, 
-                              num_neighbors=num_neighbors)
-
-v_predicted_power_knn = knn_estimation.estimate()
+knn_estimation = KnnEstimator(num_neighbors=num_neighbors)                            
+v_predicted_power_knn = knn_estimation.estimate(v_measurement_locs, v_measurements, v_x)
 
 
 # Polynomial estimation
@@ -138,65 +162,10 @@ lin_interp_estimation = LinearInterpEstimator(v_measurement_locs, v_measurements
 
 v_predicted_power_lin_interp = lin_interp_estimation.estimate()
 
-
 # DNN Estimation
-
-def data_generation_dnn(num_maps=100, num_points = num_points, y_coord = y_coord, num_samples = 20, noise_level = 0.1):
-
-    dict_true_map= {
-        'm_tx_locs': [],
-        'm_x': [],
-        'm_received_power': [],
-        'y_coord': []    
-        }
-    
-    m_noise = []
-
-    dict_sampling_mask = {
-        'm_sampling_mask': [],
-        'm_measurement_locs': []
-        }
-
-    m_measurements =[]
-    for _ in range(num_maps):    
-        m_tx_locs, v_x, v_received_power = mapGen.generate_map(num_points = num_points, y_coord = y_coord)
-
-        dict_true_map['m_tx_locs'].append(m_tx_locs)
-        dict_true_map['m_x'].append(v_x)
-        dict_true_map['m_received_power'].append(v_received_power)
-        dict_true_map['y_coord'].append(y_coord)
-
-        v_noise = mapGen.generate_noise(noise_level = noise_level, 
-                                    num_points = num_points)
-        m_noise.append(v_noise)
-        
-        v_sampling_mask, v_measurement_locs = mapGen.generate_sampling_mask(num_samples = num_samples, 
-                                                       num_points = num_points)
-        
-        dict_sampling_mask['m_sampling_mask'].append(v_sampling_mask)
-        dict_sampling_mask['m_measurement_locs'].append(v_measurement_locs)    
-        
-        m_measurements.append(mapGen.generate_measurement_data(v_x, v_received_power, v_noise, 
-                                                                          v_sampling_mask))
-    # Convert list into array in dictionary
-    dict_true_map['m_tx_locs'] = np.array(dict_true_map['m_tx_locs'])
-    dict_true_map['m_x'] = np.array(dict_true_map['m_x'])
-    dict_true_map['m_received_power'] = np.array(dict_true_map['m_received_power'])
-    dict_true_map['y_coord'] = np.array(dict_true_map['y_coord'])
-
-    m_noise = np.array(m_noise)
-
-    dict_sampling_mask['m_sampling_mask'] = np.array(dict_sampling_mask['m_sampling_mask'])
-    dict_sampling_mask['m_measurement_locs'] = np.array(dict_sampling_mask['m_measurement_locs'])
-
-    m_measurements = np.array(m_measurements)
-    
-    return dict_true_map, m_noise, dict_sampling_mask, dict_sampling_mask['m_measurement_locs'], m_measurements
-
-
-dict_true_map_train, _, _, m_measurement_locs_train, m_measurements_train = data_generation_dnn(num_maps=1000,
+m_true_map_train, _, _, m_measurement_locs_train, m_measurements_train = data_generation_dnn(num_maps=1000,
                                             num_points = num_points, y_coord = y_coord, num_samples = 20, noise_level = 0.1)
-dict_true_map_test, _, _, m_measurement_locs_test, m_measurements_test = data_generation_dnn(num_maps=30, 
+m_true_map_test, _, _, m_measurement_locs_test, m_measurements_test = data_generation_dnn(num_maps=30, 
                                             num_points = num_points, y_coord = y_coord, num_samples = 20, noise_level = 0.1)
 
 # Training and test data
@@ -231,5 +200,5 @@ new_dnn_model = DnnEstimator(input_layer_size = input_layer_size,
 new_dnn_model.load_weights(weight_file_name)
 
 # Estimate map
-m_estimated_maps = new_dnn_model.estimate(X_test_norm)
- 
+m_estimated_maps = new_dnn_model.estimate(X_test_norm) 
+"""
